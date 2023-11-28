71fc48579bf1936e2388a782092f1891669e06d2
Darien <darienjensen@outlook.com>
2023-11-28 12:45:21 -0700

    addToCart()

--- app.js
+++ app.js
@@ -1,14 +1,48 @@
-const shop = {
-    toppings:[],
-    vessels:[],
-    iceCream:[],
-}
 
 
-const item = {
+
+/**
+@typedef {{name: string, price:number, category: string, link: string}} Item
+ */
+
+
+
+let shop = [
+        toppings = [  
+            Item = {
+                name: "sprinkles",
+                price: 10,
+                category:"toppings",
+                link: ''},
+            
+        ],
+        vessels = [],
+        iceCream = [],
+]
+
+
+
+let cart = []
+
+const cart_item ={
     name:'',
+    qty:0,
     price:0,
-    category:'',
-    link:'',
 }
+// function addToShop(){
+//     let item ={
+//         name:document.
+//     }
+// }
+
+function addToCart(category, name){
+    
+    item = shop[category].find( item => item.name == name)
+
+    cart_item.name = item.name
+    cart_item.qty ++
+    cart_item.price = item.price
+    cart += cart_item
+}
+
 
